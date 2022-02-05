065c6db2ded8025000db075e63c18977b9a27a36
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-05 12:11:43 -0800

    first commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
+}
+export default PersonCard;
+
+
+// import React, { Component } from "react";
+
+// class PersonCard extends Component {
+//     render() {
+//         const { firstName, lastName, age, hairColor } = this.props;
+//         return (
+//             <div>
+//                 <h1>{ lastName } , { firstName }</h1>
+//                 <p>Age: { age }</p>
+//                 <p>Hair Color: { hairColor }</p>
+//             </div>
+//         )
+//     }
+// } 
+// export default PersonCard
